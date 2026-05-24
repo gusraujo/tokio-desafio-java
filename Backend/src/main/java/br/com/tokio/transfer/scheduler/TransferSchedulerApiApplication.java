ab7566875f66771acc80d8242d39ba667620b2cd
gusgraujo <gusg.raujo@gmail.com>
2026-05-24 03:01:58 -0300

    config: adjust config files to run application on 8082 port

---
+++ Backend/src/main/java/br/com/tokio/transfer/scheduler/TransferSchedulerApiApplication.java
@@ -0,0 +1,12 @@
+package br.com.tokio.transfer.scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransferSchedulerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransferSchedulerApiApplication.class, args);
+    }
+}
---
+++ Backend/src/test/java/br/com/tokio/transfer/scheduler/TransferSchedulerApiApplicationTests.java
@@ -0,0 +1,12 @@
+package br.com.tokio.transfer.scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransferSchedulerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
