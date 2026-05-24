188c8059ca01a6428adffa969d8997b51f359e01
gusgraujo <gusg.raujo@gmail.com>
2026-05-24 03:48:32 -0300

    feat: add scheduled transfer persistence mapper

---
+++ Backend/src/main/java/br/com/tokio/transfer/scheduler/mapper/ScheduledTransferMapper.java
@@ -0,0 +1,33 @@
+package br.com.tokio.transfer.scheduler.mapper;
+
+import br.com.tokio.transfer.scheduler.entity.ScheduledTransferEntity;
+import br.com.tokio.transfer.scheduler.model.ScheduledTransfer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScheduledTransferMapper {
+
+    public ScheduledTransferEntity toEntity(ScheduledTransfer scheduledTransfer) {
+        return new ScheduledTransferEntity(
+                scheduledTransfer.getId(),
+                scheduledTransfer.getSourceAccount(),
+                scheduledTransfer.getDestinationAccount(),
+                scheduledTransfer.getAmount(),
+                scheduledTransfer.getFee(),
+                scheduledTransfer.getTransferDate(),
+                scheduledTransfer.getSchedulingDate()
+        );
+    }
+
+    public ScheduledTransfer toDomain(ScheduledTransferEntity entity) {
+        return new ScheduledTransfer(
+                entity.getId(),
+                entity.getSourceAccount(),
+                entity.getDestinationAccount(),
+                entity.getAmount(),
+                entity.getFee(),
+                entity.getTransferDate(),
+                entity.getSchedulingDate()
+        );
+    }
+}
